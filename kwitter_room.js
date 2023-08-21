5e0662c8e65f0e125bdf16b1a3dca528e0aefa36
Brett-Marshal <131263863+Brett-Marshal@users.noreply.github.com>
2023-08-21 17:49:22 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCo7cFLKl7rs_R-d2PPz30qRbY6cx7oExg",
+    authDomain: "kwitter-14bd6.firebaseapp.com",
+    databaseURL: "https://kwitter-14bd6-default-rtdb.firebaseio.com",
+    projectId: "kwitter-14bd6",
+    storageBucket: "kwitter-14bd6.appspot.com",
+    messagingSenderId: "163848614658",
+    appId: "1:163848614658:web:ebf1c1b6686fc8731ec81b"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
